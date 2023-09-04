d1c4ec05d91a83e773baa0afe0812bbd7b097a2d
Krier <vinoshan@outlook.com>
2023-09-04 15:50:58 -0400

    Updated server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
+const Sequelize = require('sequelize');
+require('dotenv').config(); 
 
+const { Category, Product, Tag, ProductTag } = require('./models'); 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -10,7 +12,32 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
+// Database connection
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: 'localhost', 
+  dialect: 'mysql',
+});
+
+// Test the database connection
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Database connection established successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+// Sync Sequelize models to the database
+sequelize.sync({ force: false })
+  .then(() => {
+    console.log('All models are synchronized with the database.');
+  })
+  .catch((err) => {
+    console.error('An error occurred while syncing the models:', err);
+  });
+
+// Start the server
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
