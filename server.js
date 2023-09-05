ebf90c7243ca8af1e9b7369fff736faf412ac327
Krier <vinoshan@outlook.com>
2023-09-04 22:28:02 -0400

    fixed server.js

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
-const Sequelize = require('sequelize');
-require('dotenv').config(); 
+const sequelize = require('./config/connection');
 
-const { Category, Product, Tag, ProductTag } = require('./models'); 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -12,32 +10,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// Database connection
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: 'localhost', 
-  dialect: 'mysql',
-});
-
-// Test the database connection
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Database connection established successfully.');
-  })
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err);
-  });
-
-// Sync Sequelize models to the database
-sequelize.sync({ force: false })
-  .then(() => {
-    console.log('All models are synchronized with the database.');
-  })
-  .catch((err) => {
-    console.error('An error occurred while syncing the models:', err);
-  });
-
-// Start the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
 });
